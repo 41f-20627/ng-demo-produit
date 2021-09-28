7b2cf77052bb530dee8e909400e91e7fb2416e82
Jonathan Martel <jonathmartel@gmail.com>
2021-09-28 15:57:46 -0400

    avant la pause

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { AccueilComponent } from './accueil/accueil.component';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProduitComponent } from './produit/produit.component';
+import { DetailsProduitComponent } from './details-produit/details-produit.component';
+import { EnteteComponent } from './entete/entete.component';
+import { NonTrouveComponent } from './non-trouve/non-trouve.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AccueilComponent,
+    ProduitComponent,
+    DetailsProduitComponent,
+    EnteteComponent,
+    NonTrouveComponent,
   ],
   imports: [
     BrowserModule,
